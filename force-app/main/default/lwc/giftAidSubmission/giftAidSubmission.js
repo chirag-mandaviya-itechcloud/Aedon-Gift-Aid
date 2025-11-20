54008213ff98f9c4b4d3fe65cd79cd29c77a4686
chirag-mandaviya-itechcloud <chirag.mandaviya@itechcloudsolution.com>
2025-11-20 16:00:10 +0530

    changes in LWC added date pickers

--- force-app/main/default/lwc/giftAidSubmission/giftAidSubmission.js
+++ force-app/main/default/lwc/giftAidSubmission/giftAidSubmission.js
@@ -1,4 +1,39 @@
-import { LightningElement } from 'lwc';
-export default class GiftAidSubmission extends LightningElement {
+import { LightningElement, track } from 'lwc';
 
+const columns = [
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Created Date', fieldName: 'CreatedDate', type: 'date' },
+    { label: 'Paid Amount', fieldName: 'Paid_Amount__c' }
+]
+export default class GiftAidSubmission extends LightningElement {
+    @track startDate;
+    @track endDate;
+    @track errorMessage = '';
+
+    columns = columns;
+    @track SalesInvoiceTransactionData = [];
+    @track selectedRows = [];
+
+    handleDateChange(event) {
+        const fieldName = event.target.name;
+        const fieldValue = event.target.value;
+
+        if (fieldName === 'startDate') {
+            this.startDate = fieldValue;
+        }
+        if (fieldName === 'endDate') {
+            this.endDate = fieldValue;
+        }
+
+        this.validateDates();
+    }
+
+    validateDates() {
+        this.errorMessage = '';
+
+        if (this.startDate && this.endDate && this.startDate > this.endDate) {
+            this.errorMessage = 'Start date cannot be greater than end date.';
+            return;
+        }
+    }
 }
