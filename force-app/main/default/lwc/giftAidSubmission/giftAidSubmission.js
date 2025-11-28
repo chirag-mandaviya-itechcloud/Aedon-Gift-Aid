00cf09231fd3cc1597956b86e7bf24cfbff54f99
PriyankTiwari9815 <priyanktiwari@psadvisory.com>
2025-11-28 12:04:46 +0530

    changes taken for table view

--- force-app/main/default/lwc/giftAidSubmission/giftAidSubmission.js
+++ force-app/main/default/lwc/giftAidSubmission/giftAidSubmission.js
@@ -6,23 +6,22 @@ import saveSubmission from '@salesforce/apex/GiftAidSubmissionController.saveSub
 
 const columns = [
     { label: 'Sales Header', fieldName: 'salesInvoiceHeaderName', initialWidth: 150 },
-    { label: 'Name', fieldName: 'name', initialWidth: 150 },
+    // { label: 'Name', fieldName: 'name', initialWidth: 150 },
     { label: 'Company', fieldName: 'companyName', initialWidth: 150 },
     { label: 'Invoice Date', fieldName: 'invoiceDate', type: 'date', initialWidth: 150 },
     { label: 'Account Name', fieldName: 'accountName', initialWidth: 150 },
+    { label: 'First Name', fieldName: 'contactFirstName', initialWidth: 150 },
+    { label: 'Last Name', fieldName: 'contactLastName', initialWidth: 150 },
+    { label: 'Postal Code', fieldName: 'contactPostalCode', initialWidth: 150 },
     { label: 'Customer Reference', fieldName: 'customerReference', initialWidth: 150 },
     { label: 'Product Name', fieldName: 'productName', initialWidth: 150 },
     { label: 'Nominal Code', fieldName: 'nominalCode', initialWidth: 150 },
     { label: 'Sales VAT', fieldName: 'salesVAT', initialWidth: 150 },
+    { label: 'Paid Amount', fieldName: 'paidAmount', initialWidth: 150 },
     { label: 'Analysis 1', fieldName: 'analysis1', initialWidth: 150 },
     { label: 'Analysis 2', fieldName: 'analysis2', initialWidth: 150 },
     { label: 'Analysis 6', fieldName: 'analysis6', initialWidth: 150 },
     { label: 'Analysis 7', fieldName: 'analysis7', initialWidth: 150 },
-    { label: 'First Name', fieldName: 'contactFirstName', initialWidth: 150 },
-    { label: 'Last Name', fieldName: 'contactLastName', initialWidth: 150 },
-    { label: 'Postal Code', fieldName: 'contactPostalCode', initialWidth: 150 },
-    // { label: 'Created Date', fieldName: 'CreatedDate', type: 'date' },
-    { label: 'Paid Amount', fieldName: 'paidAmount', initialWidth: 150 },
 ]
 export default class GiftAidSubmission extends NavigationMixin(LightningElement) {
     @track startDate = null;
